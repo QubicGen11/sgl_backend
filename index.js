914ea819890e55c64f284aba6f026fe906598038
shaiksajidhussain <sanjusazid0@gmail.com>
2024-07-23 23:50:03 +0530

    Added the test

--- index.js
+++ index.js
@@ -20,5 +20,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
