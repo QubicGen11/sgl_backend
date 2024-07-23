41d90b006a2bd522d3686ef76ec27f0c9be3d9b0
shaiksajidhussain <sanjusazid0@gmail.com>
2024-07-24 02:20:26 +0530

    removed slash

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://sgl.vercel.app/', 
+  origin: 'https://sgl.vercel.app', 
   // origin:'https://sgl.vercel.app', // Add your frontend URLs here
   credentials: true
 }));
