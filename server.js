ab09d8c7e9b7f8e5557e8fe3edb25053fa0b3b2e
shaiksajidhussain <sanjusazid0@gmail.com>
2024-07-24 11:32:40 +0530

    added the code

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const feedbackRoutes = require('./routes/feedback');
 
 const app = express();
 
-// Allow all origins
-app.use(cors());
+const corsOptions = {
+  origin: ['https://sgl.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use('/api/feedback', feedbackRoutes);
